c5118e9d6510d50e31da5fc8a134faa69cef2ccf
weberl48 <weberl48@outlook.com>
2016-08-14 13:56:33 -0400

    api call action

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import SearchBar from '../containers/serach_bar'
+
+export default class App extends Component {
+  render() {
+    return (
+      <SearchBar />
+    );
+  }
+}
